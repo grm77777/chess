5b67391f1a69ce9c7a83c28163fd0ecac688d05e
Genevieve <grm777@byu.edu>
2025-05-30 15:48:40 -0700

    Implemented user SQL table

---
+++ server/src/test/java/dataaccess/SQLAuthDAOTests.java
@@ -0,0 +1,66 @@
+package dataaccess;
+
+import dataaccess.MySQLDAO.MySQLUserDAO;
+import model.UserData;
+import org.junit.jupiter.api.*;
+
+public class SQLAuthDAOTests {
+
+    private final UserDAO userDAO = new MySQLUserDAO();
+
+    @BeforeAll
+    public static void setUp() {
+        Assertions.assertDoesNotThrow(DatabaseManager::configureDatabase, "Threw database error.");
+    }
+
+    @BeforeEach
+    public void clear() {
+        userDAO.clearAllUsers();
+    }
+
+    @Test
+    @Order(1)
+    public void createValidUser() {
+        UserData expected = new UserData("username", "password", "email");
+        userDAO.createUser("username", "password", "email");
+        UserData actual = userDAO.getUser("username");
+        Assertions.assertEquals(expected, actual, "The user wasn't found in the database.");
+    }
+
+    @Test
+    @Order(2)
+    public void createInvalidUser() {
+        userDAO.createUser("username", "password", "email");
+        Assertions.assertThrows(RuntimeException.class,
+                () -> userDAO.createUser("username", "password", "email"));
+    }
+
+    @Test
+    @Order(3)
+    public void getValidUser() {
+        UserData expected = new UserData("username", "password", "email");
+        userDAO.createUser("username", "password", "email");
+        UserData actual = userDAO.getUser("username");
+        Assertions.assertEquals(expected, actual, "The user wasn't found in the database.");
+    }
+
+    @Test
+    @Order(4)
+    public void getInvalidUser() {
+        userDAO.createUser("username", "password", "email");
+        UserData user = userDAO.getUser("username1");
+        Assertions.assertNull(user);
+    }
+
+    @Test
+    @Order(5)
+    public void clearUsers() {
+        userDAO.createUser("username", "password", "email");
+        userDAO.createUser("username1", "password1", "email1");
+        userDAO.createUser("username2", "password2", "email2");
+        Assertions.assertNotNull(userDAO.getUser("username"));
+        userDAO.clearAllUsers();
+        UserData user = userDAO.getUser("username");
+        Assertions.assertNull(user);
+    }
+}
