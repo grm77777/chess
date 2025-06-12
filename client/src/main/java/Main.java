7641b01651c11cb73aeb8ce12afc65f41bf8ae8c
Genevieve <grm777@byu.edu>
2025-06-11 20:57:20 -0600

    Fixed URL

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,9 +1,8 @@
-import chess.*;
 import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
-        Repl repl = new Repl("localhost:8080");
+        Repl repl = new Repl("http://localhost:8080");
         repl.run();
     }
 }
