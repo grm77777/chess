d179309e1e969dc907f9d93eec7263edf3547a64
Genevieve <grm777@byu.edu>
2025-06-12 16:27:01 -0600

    Implemented register

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,22 @@
 package client;
 
+import dataaccess.mysql.MySQLUserDAO;
 import org.junit.jupiter.api.*;
 import server.Server;
+import service.ClearService;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String serverURL = "http://localhost:" + port;
+        serverFacade = new ServerFacade(serverURL);
     }
 
     @AfterAll
@@ -19,9 +24,26 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void setup() {
+        var clearService = new ClearService();
+        clearService.clear();
+    }
+
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerRequest() {
+        serverFacade.register("username", "password", "email");
+        var userDAO = new MySQLUserDAO();
+        var actual = userDAO.getUser("username").username();
+        var expected = "username";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void registerAlreadyTaken() {
+        serverFacade.register("username", "password", "email");
+        Assertions.assertThrows(ResponseException.class, () -> serverFacade.register(
+                "username", "password", "email"));
     }
 
 }
