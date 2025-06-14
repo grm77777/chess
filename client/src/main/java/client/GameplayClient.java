1101a25be97c5681095328758e8db0d908a8b7b9
Genevieve <grm777@byu.edu>
2025-06-14 17:28:05 -0600

    Phase 6 setup

---
+++ client/src/main/java/client/GameplayClient.java
@@ -0,0 +1,18 @@
+package client;
+
+public class GameplayClient implements Client {
+
+    public GameplayClient() {
+
+    }
+
+    @Override
+    public String openingMessage() {
+        return "";
+    }
+
+    @Override
+    public String eval(String input) {
+        return "";
+    }
+}
